de338c04d2a905c8ecb7a1d215fcd59bb4a3af72
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-30 18:57:55 -0700

    mobile responsiveness, prettier, Link fix

--- src/pages/localism.tsx
+++ src/pages/localism.tsx
@@ -1,15 +1,50 @@
-import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react'
-import { Page, ButtonLink } from '@/components'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { ButtonLink, Link, Page } from '@/components'
 
 const Localism = () => (
-  <Page title="GFEL" description="A General Forum on Ethereum Localism" minH="100vh" pb={32}>
+  <Page title="GFEL" description="A General Forum on Ethereum Localism" pb={32}>
     <Flex direction="column" alignItems="center" justifyContent="center">
-      <Box style={{ background: "url(../assets/gfel_banner-noText.png) center center no-repeat rgba(0,0,0,0.5)", backgroundSize: "cover"}} backgroundBlendMode="multiply" minH="420px" w="100%" p={24} position="relative">
-        <Flex direction="column" border="2px" borderColor="#05FF00" borderRadius="25px" alignItems="center" justifyContent="center" zIndex={-1}>
-          <Text as="h1" textStyle="h1" fontSize="6xl" fontFamily="serif" color="#05FF00">
-            A General Forum on <br />Ethereum Localism
+      <Box
+        style={{
+          background:
+            'url(../assets/gfel_banner-noText.png) center center no-repeat rgba(0,0,0,0.5)',
+          backgroundSize: 'cover',
+        }}
+        backgroundBlendMode="multiply"
+        minH="420px"
+        w="100%"
+        p={{ base: 12, md: 24 }}
+        position="relative"
+      >
+        <Flex
+          direction="column"
+          border="2px"
+          borderColor="secondary"
+          borderRadius="25px"
+          alignItems="center"
+          justifyContent="center"
+          zIndex={-1}
+        >
+          <Text
+            as="h1"
+            textStyle="h1"
+            fontSize={{ base: '4xl', md: '6xl' }}
+            fontFamily="serif"
+            color="secondary"
+            px={8}
+          >
+            A General Forum on <br />
+            Ethereum Localism
           </Text>
-          <Flex direction={['column', null, null, 'row']} gap={[0, null, null, 9]} color="#05FF00" justifyContent="center" alignItems="center" w="100%" flexWrap="wrap">
+          <Flex
+            direction={['column', null, null, 'row']}
+            gap={[0, null, null, 9]}
+            color="secondary"
+            justifyContent="center"
+            alignItems="center"
+            w="100%"
+            flexWrap="wrap"
+          >
             <Text fontSize="2xl" fontWeight="900">
               Friday the 13th
             </Text>
@@ -24,48 +59,90 @@ const Localism = () => (
       </Box>
       <Flex justify="center" bg="secondary" boxShadow="lg" w="100vw">
         <Flex
-          py={4}
+          p={4}
           alignItems="center"
           position="relative"
           justifyContent="center"
-          direction={[ 'column', null, null, 'row' ]}
+          direction={['column', null, null, 'row']}
           gap={6}
           maxW="max"
         >
           <Heading>Applications Open</Heading>
-          <Text m={0}>Our space is lovely but small, so capacity is limited to 100, so please read up on the intention below before applying.</Text>
+          <Text m={0}>
+            Our space is lovely but small, so capacity is limited to 100, so
+            please read up on the intention below before applying.
+          </Text>
           <ButtonLink href="https://mirror.xyz/ethpdx.eth/kjpsLAAC2Si0XDmr_aFp0F5esPNH4DoPB4lOTlFbR5M">
             Apply Now
           </ButtonLink>
         </Flex>
       </Flex>
-      <Flex direction="column" maxW="720px" gap={12}>
-        <Box pt={12}>
-          <Text fontSize="xl">Our intention for this convening is to create a container where the Ethereum community can elaborate for itself, in a plurality of directions, what an experimentalism of the city can mean and look like for web3.</Text>
-          <ButtonLink href="https://mirror.xyz/ethpdx.eth/kjpsLAAC2Si0XDmr_aFp0F5esPNH4DoPB4lOTlFbR5M">Read More</ButtonLink>
+      <Flex direction="column" maxW="720px" gap={12} pt={12} px={4}>
+        <Box>
+          <Text fontSize="xl">
+            Our intention for this convening is to create a container where the
+            Ethereum community can elaborate for itself, in a plurality of
+            directions, what an experimentalism of the city can mean and look
+            like for web3.
+          </Text>
+          <ButtonLink href="https://mirror.xyz/ethpdx.eth/kjpsLAAC2Si0XDmr_aFp0F5esPNH4DoPB4lOTlFbR5M">
+            Read More
+          </ButtonLink>
         </Box>
         <Box>
-          <Heading textStyle="h2">Details and Information for Attendees</Heading>
-          <Heading textStyle="h2">Location</Heading>
-          <Text><strong>Venue: BridgeSpace</strong></Text>
+          <Heading as="h2" textStyle="h2">
+            Details and Information for Attendees
+          </Heading>
+          <Heading as="h3" textStyle="h2">
+            Location
+          </Heading>
+          <Text fontWeight="bold">Venue: BridgeSpace</Text>
           <Text>133 SE Madison St, Portland, OR 97214</Text>
-          <ButtonLink href="https://goo.gl/maps/E4pmDRBsjC8nRDuM6" mb={6}>View on Google Maps</ButtonLink>
+          <ButtonLink href="https://goo.gl/maps/E4pmDRBsjC8nRDuM6" mb={6}>
+            View on Google Maps
+          </ButtonLink>
           <Text fontWeight="900">Where to Stay</Text>
-          <Text>The venue is very central, on the east side of the Willamette River. Airbnb is a great option and there should be plenty of availabilities in the neighborhoods near the venue (Central Eastside Industrial District (CEID), Hawthorne, Ladd&apos;s Addition, etc)</Text>
+          <Text>
+            The venue is very central, on the east side of the Willamette River.
+            Airbnb is a great option and there should be plenty of
+            availabilities in the neighborhoods near the venue (Central Eastside
+            Industrial District (CEID), Hawthorne, Ladd&apos;s Addition, etc)
+          </Text>
         </Box>
         <Box>
-          <Heading textStyle="h2">Getting Around</Heading>
-          <Text>Portland is filled with walkable neighborhoods, and is also very bikeable. An Uber or Lyft can get you anywhere else in a hurry.</Text>
+          <Heading as="h2" textStyle="h2">
+            Getting Around
+          </Heading>
+          <Text>
+            Portland is filled with walkable neighborhoods, and is also very
+            bikeable. An Uber or Lyft can get you anywhere else in a hurry.
+          </Text>
           <Flex direction="column">
-            <Text><strong>Bike Rentals</strong></Text>
-            <Link href="https://www.pdxbikerentals.com/bicycle-rentals-portland#bike-rentals-portland" target="_blank">Everybody&apos;s Bike Rentals</Link>
-            <Link href="https://biketownpdx.com/" target="_blank">Biketown</Link>
+            <Text fontWeight="bold">Bike Rentals</Text>
+            <Link href="https://www.pdxbikerentals.com/bicycle-rentals-portland#bike-rentals-portland">
+              Everybody&apos;s Bike Rentals
+            </Link>
+            <Link href="https://biketownpdx.com/">Biketown</Link>
           </Flex>
         </Box>
         <Box>
-          <Heading textStyle="h2">Eats, Drinks, and Hangs</Heading>
-          <Text>Portland is well-known for its food and generally artisan quality everything.</Text>
-          <Text>For quick eats, there are many food cart pods around the city. The closest to the venue being <Link href="https://goo.gl/maps/55CpzWHsHUHZjBXs6" target="_blank" rel="noopener noreferrer">the Asylum</Link></Text>
+          <Heading as="h2" textStyle="h2">
+            Eats, Drinks, and Hangs
+          </Heading>
+          <Text>
+            Portland is well-known for its food and generally artisan quality
+            everything.
+          </Text>
+          <Text>
+            For quick eats, there are many food cart pods around the city. The
+            closest to the venue being{' '}
+            <Link
+              href="https://goo.gl/maps/55CpzWHsHUHZjBXs6"
+              rel="noopener noreferrer"
+            >
+              the Asylum
+            </Link>
+          </Text>
         </Box>
         {/* <Box>
           <Heading textStyle="h2">Things to Do</Heading>
