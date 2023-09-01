01faece24a7d384519fd4b5e4ff8cef740428c97
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-31 18:37:20 -0700

    update next.config trailingSlash=true

--- src/components/LocalismBanner.tsx
+++ src/components/LocalismBanner.tsx
@@ -16,7 +16,7 @@ export const LocalismBanner = () => (
         October 16th, 2023.
       </Text>
       <ButtonLink
-        href="/localism"
+        href="/localism/"
         whiteSpace="nowrap"
         me={8}
         bg="transparent"
